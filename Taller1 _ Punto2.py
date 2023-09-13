52f033b757154f93758ae5e164c59cde02f22e97
irjimenezv <112517255+irjimenezv@users.noreply.github.com>
2023-09-13 12:41:49 -0500

    Add files via upload

---
+++ Taller1 _ Punto2.py
@@ -0,0 +1,12 @@
+x= float(input())
+y= float(input())
+z= float(input())
+
+if x > y and x > z:
+    print("El numero mayor es:",(x))
+    
+if y > x and y > z:
+    print("El numero mayor es:",(y))
+
+if z > y and z > x:
+    print("El numero mayor es:",(z))
