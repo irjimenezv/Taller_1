689daf4148420c28cfba7f923e5f099bde75005a
irjimenezv <112517255+irjimenezv@users.noreply.github.com>
2023-09-25 19:36:22 -0500

    Add files via upload

---
+++ Taller1_Punto5.py
@@ -0,0 +1,12 @@
+x= float(input())
+y= float(input())
+z= float(input())
+
+if x + y == z:
+    print("la suma x+y es igual a z")
+
+elif x + y > z:
+    print("la suma x+y es mayor a z")
+
+else:
+    print("la suma x+y es menor a z")
