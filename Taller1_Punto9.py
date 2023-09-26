1323b052a560774dbbf478887597bdb0e2efe43e
irjimenezv <112517255+irjimenezv@users.noreply.github.com>
2023-09-25 22:23:24 -0500

    Add files via upload

---
+++ Taller1_Punto9.py
@@ -0,0 +1,40 @@
+x= str(input())
+
+if x == ("colombia"):
+    print("Bogota")
+
+elif x== ("venezuela"):
+    print("caracas")
+    
+elif x== ("argentina"):
+    print("buenos aires")
+    
+elif x== ("ecuador"):
+    print("quito")
+    
+elif x== ("peru"):
+    print("lima")
+    
+elif x== ("brasil"):
+    print("brasilia")
+    
+elif x== ("bolivia"):
+    print("sucre")
+    
+elif x== ("paraguay"):
+    print("asuncion")
+    
+elif x== ("chile"):
+    print("santiago de chile")
+    
+elif x== ("uruguay"):
+    print("montevideo")
+    
+elif x== ("surinam"):
+    print("paramaribo")
+    
+elif x== ("guyana"):
+    print("georgetown")
+    
+else:
+    print("pais no identificado")
